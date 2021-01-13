5d971c329b9ba5c377cf0b369e891982b0a15b99
Richard Clowes <Richard.Clowes@sas.com>
2021-01-13 08:55:50 -0500

    testing

--- src/code/views/SAS3view.tsx
+++ src/code/views/SAS3view.tsx
@@ -5,41 +5,62 @@ const eventBus: AbstractModel = new AbstractModel();
 import { context } from "../Context";
 import { NativeSabreCommand } from "../services/NativeSabreCommand";
 import { Alert } from "react-bootstrap";
+import { IReservationService } from "sabre-ngv-reservation/services/IReservationService";
+import {
+  CommandMessageReservationRs,
+  ReservationRs,
+} from "sabre-ngv-pos-cdm/reservation";
 
 export interface MyProps {
   closePopovers: () => void;
 }
 
 export interface MyState {
-  command?: string;
-  soccer?: string;
-  firstName?: string;
-  responseMessage?: string;
-  lastName?: string;
-  isError?: boolean;
-  jsonData?: any;
-  isLoading?: boolean;
-  lastIndex?: number;
-  filename?: string;
+  remarks?: any;
 }
 
 export class SAS3view extends React.Component<MyProps, MyState> {
   constructor(props: MyProps) {
     super(props);
-
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.closePopovers = this.closePopovers.bind(this);
     this.state = {
-      command: "5test",
-      soccer: "Man City",
-      responseMessage: "",
-      jsonData: [],
-      isLoading: false,
-      isError: false,
-      lastIndex: 0,
-      firstName: "",
-      lastName: "",
-      filename: "jsonRefPoints.sas&action=getRefPoints&isActive=1",
+      remarks: [],
     };
+  }
+
+  // getting pnr QC remarks
+  componentDidMount() {
+    console.log("Getting QC remarks....");
+    let reservationPromise: Promise<CommandMessageReservationRs> = getService(
+      IReservationService
+    ).getReservation();
+    reservationPromise.then(this.displayRemarks.bind(this)).catch((error) => {
+      console.log("Error while receiving reservation");
+      console.log(error);
+    });
+  }
+
+  private displayRemarks(
+    commandMessageReservationRs: CommandMessageReservationRs
+  ): void {
+    var reservation = commandMessageReservationRs["Data"].Remarks.Remark;
+    console.log(`reservation=${reservation}`);
+    // only keep code=Q
+    const remarks: any = reservation.filter(function (r) {
+      return r.Code && r.Code === "Q";
+    });
+    console.log(`remarks=${remarks}`);
+
+    remarks.sort(function (remark1, remark2) {
+      return remark1.OrderNumber - remark2.OrderNumber;
+    });
+
+    this.setState({
+      remarks: remarks,
+    });
+    console.log(remarks);
   }
 
   private closePopovers() {
@@ -47,12 +68,23 @@ export class SAS3view extends React.Component<MyProps, MyState> {
     eventBus.triggerOnEventBus("hide-popovers", "novice-menu");
   }
 
-  private handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const value: string = event.currentTarget.value;
-    const name: string = event.currentTarget.name;
-    console.log(`Changing ${name} to ${value}`);
+  private handleChange = (id: number) => (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const name: any = e.target.name;
+    let value: any;
+    if (name === "Keep") {
+      value = e.target.checked;
+    } else {
+      value = e.target.value;
+    }
+    console.log(`${value}, ${name}, ${id}`);
+    const responses = this.state.remarks.map((i) =>
+      i.Id == id ? { ...i, [name]: value } : i
+    );
+
     this.setState({
-      [name]: value,
+      remarks: responses,
     });
   };
 
@@ -63,13 +95,46 @@ export class SAS3view extends React.Component<MyProps, MyState> {
     this.closePopovers();
   };
 
+  public handleSubmit = async (
+    event?: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event && event.preventDefault();
+    console.log(`I clicked submit ${event}`);
+  };
+
   // get queue info
 
   render(): JSX.Element {
     return (
       <div className="tab-pane" id="3">
-        <h1>Content About #3</h1>
-        <p>Three is most definitely the loneliest number don't you think?</p>
+        <h3>Quality Control Remarks</h3>
+        <div className="qcremarks">
+          <form id="QCForm" onSubmit={this.handleSubmit} ref="form">
+            {this.state.remarks.map((s) => (
+              <div key={s.Id} className="form-row">
+                <div className="form-group col-md-1">
+                  <input
+                    type="checkbox"
+                    name="Keep"
+                    checked={s.Keep}
+                    onChange={this.handleChange(s.Id)}
+                  />
+                </div>
+
+                <div className="form-group col-md-11">
+                  <input
+                    name="Text"
+                    placeholder="Text Remark"
+                    type="text"
+                    className="form-control"
+                    value={s.Text}
+                    onChange={this.handleChange(s.Id)}
+                  />
+                </div>
+              </div>
+            ))}
+          </form>
+        </div>
       </div>
     );
   }
